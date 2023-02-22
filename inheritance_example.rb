7d715fa5805034047c90e9754c9d8055dc89b24c
“lukenilsson” <luke.nilsson@gmail.com>
2023-02-22 15:02:33 -0500

    raise / fire employees

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
   def initialize
     @speed = 0
     @direction = "north"
@@ -15,22 +15,56 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
+
+class Car < Vehicle
+  def initialize
+    super
+    @model = "Honda"
+    @make = "make"
+  end
 
   def honk_horn
     puts "Beeeeeeep!"
   end
+
+  def fuel
+    @fuel
+  end
+
+  def make
+    @make
+  end
 end
 
-class Bike < Car
+class Bike < Vehicle
+  def initialize
+    super
+    @type = "mountain"
+    @weight = 25
+  end
+
   def ring_bell
     puts "Ring ring!"
+  end
+
+  def type
+    @type
+  end
+
+  def weight
+    @weight
   end
 end
 
 car = Car.new
-p car.accelerate
-car.honk_horn
-
 bike = Bike.new
-p bike.accelerate
+
+p bike
+bike.accelerate
+bike.accelerate
+bike.accelerate
+bike.accelerate
 bike.ring_bell
+car.honk_horn
+p bike
